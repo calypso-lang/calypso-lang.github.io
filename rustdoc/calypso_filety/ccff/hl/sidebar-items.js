b789e3bfcb2f85eeca4a165d3b7e55fdb10aacbc
Travis CI <deploy@travis-ci.org>
2021-02-21 07:20:14 +0000

    Deploy calypso-lang/calypso to github.com/calypso-lang/calypso-lang.github.io.git:main

---
+++ rustdoc/calypso_filety/ccff/hl/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["ContainerFile",""],["Section",""]]});
--- rustdoc/source-files.js
+++ rustdoc/source-files.js
@@ -2,6 +2,8 @@ var N = null;var sourcesIndex = {};
 sourcesIndex["calypso"] = {"name":"","dirs":[{"name":"commands","files":["explain.rs","internal.rs","mod.rs"]}],"files":["main.rs","messages.rs"]};
 sourcesIndex["calypso_base"] = {"name":"","dirs":[{"name":"streams","files":["generic.rs","str.rs"]}],"files":["lib.rs","macros.rs","span.rs","streams.rs"]};
 sourcesIndex["calypso_diagnostic"] = {"name":"","files":["diagnostic.rs","error.rs","lib.rs","report.rs","types.rs"]};
+sourcesIndex["calypso_filety"] = {"name":"","dirs":[{"name":"ccff","files":["hl.rs","ll.rs"]}],"files":["ccff.rs","lib.rs"]};
+sourcesIndex["calypso_opt"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["calypso_parsing"] = {"name":"","dirs":[{"name":"lexer","files":["helpers.rs","ident_kw.rs","lit.rs","scan.rs","types.rs","ws.rs"]},{"name":"pretty","files":["lexer.rs","mod.rs"]}],"files":["lexer.rs","lib.rs"]};
 sourcesIndex["calypso_repl"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["calypso_util"] = {"name":"","files":["lib.rs"]};

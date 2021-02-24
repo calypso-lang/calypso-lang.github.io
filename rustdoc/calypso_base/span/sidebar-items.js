cef84256fbaad6a333e459bcf1c5bbc654c9f45b
Travis CI <deploy@travis-ci.org>
2021-02-24 04:22:06 +0000

    Deploy calypso-lang/calypso to github.com/calypso-lang/calypso-lang.github.io.git:main

--- rustdoc/calypso_base/span/sidebar-items.js
+++ rustdoc/calypso_base/span/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Span","The location in a slice in which some object spans."],["Spanned","An object that's associated with a `Span`"]]});
+initSidebarItems({"struct":[["Span","The location in a slice in which some object spans."],["Spanned","An object that’s associated with a `Span`"]]});
